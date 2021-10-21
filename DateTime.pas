3af259f1f2104f033ebaca29ca5e85d21a7a2f31
Anastasia <89418251+sicknostya@users.noreply.github.com>
2021-10-21 19:53:56 +0300

    add isleapyear

--- DateTime.pas
+++ DateTime.pas
@@ -1,2 +1,11 @@
-Begin
-End.
+function IsLeapYear(y: integer): boolean;
+begin
+    if (y mod 4 = 0) and (y mod 100 <> 0) then
+        result := True;
+    if y mod 400 = 0 then result := True;
+end;
+
+
+begin
+  
+end.
